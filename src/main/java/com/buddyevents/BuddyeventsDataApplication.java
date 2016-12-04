6822d9034154f4e9f4eee8201bcecc3291c732db
syamprasad.guda syamprasad.guda <syamprasad.guda syamprasad.guda>
2016-12-04 16:09:34 -0500

    initil commit

---
+++ src/main/java/com/buddyevents/BuddyeventsDataApplication.java
@@ -0,0 +1,12 @@
+package com.buddyevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyeventsDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyeventsDataApplication.class, args);
+	}
+}
